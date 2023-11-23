08b2d471810d5b435743e9a26ffef999944bbe06
arturssans <139260685+arturssans@users.noreply.github.com>
2023-11-23 08:18:22 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -30,9 +30,10 @@ function alterarNomePersonagemSelecionado(personagem) {
 }
 
 function alterarImagemPersonagemSelecionado(personagem) {
-    const imagemPersonagemGrande = document.querySelector('personagem-grande');
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    // passo 2 - alterar a imagem do personagem grande
     const idPersonagem = personagem.attributes.id.value;
-    imagemPersonagemGrande.src = './src/Imagens/${idPersonagem}.png';
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
 }
 
 function removerSelecaoDoPersonagem() {
